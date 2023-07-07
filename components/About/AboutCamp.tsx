16bafa3445507ff30c0167c30005b09c9e72ff3a
DenserMeerkat <95911940+DenserMeerkat@users.noreply.github.com>
2023-07-07 16:36:45 +0530

    Completed AboutCamp
    -Added CampCards
    -Styed CampCards

--- components/About/AboutCamp.tsx
+++ components/About/AboutCamp.tsx
@@ -1,19 +1,16 @@
 "use client";
 import React, { useState } from "react";
 import SectionHeading from "../common/SectionHeading";
-import { Tent } from "lucide-react";
-import { buttonCSS } from "../common/tailwindCSS";
+import { Tent, Text, List, Hourglass } from "lucide-react";
 import camps from "../resources/camps";
+import { CampButton, CampCard } from "./CampComps";
 
 const AboutCamp = () => {
   const [active, setActive] = useState(0);
-  const handleClick = (index: number) => {
-    setActive(index);
-  };
   return (
     <div className=" relative h-max py-10">
       <div className=" max-w-5xl mx-auto">
-        <SectionHeading icon={Tent} title="Camps in NCC" className={"mb-6"} />
+        <SectionHeading icon={Tent} title="Camps in NCC" className={"mb-4"} />
         <div className="grid grid-cols-12 gap-4">
           <div className="md:col-span-5 p-4 flex flex-col gap-4">
             <h3 className="text-3xl font-bold">
@@ -35,12 +32,26 @@ const AboutCamp = () => {
                   id={camp.id}
                   title={camp.shortTitle}
                   active={active === index}
-                  onClick={handleClick}
+                  onClick={setActive}
                 />
               ))}
             </ul>
           </div>
-          <div className="md:col-span-7 border-2 p-4 rounded-xl"></div>
+          <div className="md:col-span-7 p-4">
+            <ul>
+              {camps.map((camp: any, index: number) => (
+                <CampCard
+                  key={camp.id}
+                  active={active === index}
+                  title={camp.title}
+                  shortTitle={camp.shortTitle}
+                  duration={camp.duration}
+                  desc={camp.desc}
+                  activities={camp.activities}
+                />
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
@@ -48,33 +59,3 @@ const AboutCamp = () => {
 };
 
 export default AboutCamp;
-
-export const CampButton = (props: any) => {
-  //Props
-  const id = props.id;
-  const title = props.title;
-  const active = props.active;
-  const onClick = props.onClick;
-  //Tailwind CSS
-  const buttonTail = `border rounded-md col-span-3 flex px-6 py-3 transition-colors select-none`;
-  const activeTail = `bg-sky-200/[0.8] dark:bg-slate-800/[0.9] text-sky-950 dark:text-slate-100 border-sky-300 dark:border-slate-700 `;
-  const iconTail = `p-0.5 mr-3`;
-  const inactiveIconTail = `text-sky-900/[0.5] dark:text-sky-300/[0.5]`;
-  const activeIconTail = `text-sky-600 dark:text-sky-400`;
-  const inactiveTitleTail = `font-medium text-sky-900/[0.8] dark:text-slate-200/[0.8]`;
-  const activeTitleTail = `font-bold text-sky-600 dark:text-sky-400`;
-  return (
-    <button
-      key={id}
-      className={`${buttonTail} ${active ? activeTail : ""}`}
-      onClick={() => onClick(id)}
-    >
-      <Tent
-        className={`${iconTail} ${active ? activeIconTail : inactiveIconTail}`}
-      />
-      <h4 className={`${active ? activeTitleTail : inactiveTitleTail}`}>
-        {title}
-      </h4>
-    </button>
-  );
-};
---
+++ components/About/CampComps.tsx
@@ -0,0 +1,115 @@
+import { Tent, Text, List, Hourglass } from "lucide-react";
+
+const CampButton = (props: any) => {
+  //Props
+  const id = props.id;
+  const title = props.title;
+  const active = props.active;
+  const onClick = props.onClick;
+
+  //Tailwind CSS
+  const buttonTail = `border rounded-md col-span-3 flex px-6 py-3 transition-colors select-none`;
+  const activeTail = `bg-sky-200/[0.8] dark:bg-slate-800/[0.9] text-sky-950 dark:text-slate-100 border-sky-300 dark:border-slate-700 `;
+
+  const iconTail = `p-0.5 mr-3`;
+  const inactiveIconTail = `text-sky-900/[0.5] dark:text-sky-300/[0.5]`;
+  const activeIconTail = `text-sky-600 dark:text-sky-400`;
+
+  const inactiveTitleTail = `font-medium text-sky-900/[0.8] dark:text-slate-200/[0.8]`;
+  const activeTitleTail = `font-bold text-sky-600 dark:text-sky-400`;
+
+  //Render
+  return (
+    <button
+      key={id}
+      className={`${buttonTail} ${active ? activeTail : ""}`}
+      onClick={() => {
+        console.log(id);
+        onClick(id);
+      }}
+    >
+      <Tent
+        className={`${iconTail} ${active ? activeIconTail : inactiveIconTail}`}
+      />
+      <h4 className={`${active ? activeTitleTail : inactiveTitleTail}`}>
+        {title}
+      </h4>
+    </button>
+  );
+};
+
+type CampCardProps = {
+  activities: string[];
+  shortTitle: string;
+  duration: string;
+  desc: string;
+  active: boolean;
+};
+
+const CampCard: React.FC<CampCardProps & React.HTMLProps<HTMLDivElement>> = ({
+  activities,
+  ...props
+}) => {
+  //Props
+  const id = props.id;
+  const title = props.title;
+  const shortTitle = props.shortTitle;
+  const duration = props.duration;
+  const desc = props.desc;
+  const active = props.active;
+
+  //Tailwind CSS
+  const cardTail = `border-2 rounded-md p-4 bg-sky-100 dark:bg-slate-900 dark:border-slate-800 opacity-100 transition-opacity duration-500`;
+  const inactiveCardTail = `hidden opacity-0 transition-opacity duration-500`;
+
+  const titleTail = `text-2xl font-bold text-sky-900 dark:text-sky-200`;
+  const shortTitleTail = `mr-3 px-2 py-0.5 rounded-lg text-base font-bold border
+      border-sky-400 dark:border-sky-600 bg-sky-200 dark:bg-sky-900 text-sky-950 dark:text-sky-200`;
+
+  const iconTail = `p-1 mr-2 pl-0 text-rose-800 dark:text-rose-300/[0.7]`;
+  const subHeadingTail = `text-base font-bold text-rose-900 dark:text-rose-300/[0.7]`;
+
+  const descTail = `pl-2 dark:text-sky-200/[0.7] mb-4`;
+  const activityTail = `pl-2 dark:text-sky-200/[0.7]`;
+
+  function subHeading(Icon: any, title: string) {
+    return (
+      <div className="flex items-center mb-2 ">
+        <Icon className={iconTail} />
+        <h4 className={subHeadingTail}>{title}</h4>
+      </div>
+    );
+  }
+  //Render
+  return (
+    <div key={id} className={active ? cardTail : inactiveCardTail}>
+      <div className="flex items-center mb-2 pl-2 pb-3 border-b">
+        <p className={shortTitleTail}>{shortTitle}</p>
+        <h3 className={titleTail}>{title}</h3>
+      </div>
+      <div className="flex justify-end"></div>
+      {subHeading(Text, "Description")}
+      <p className={descTail}>{desc}</p>
+      <div className="flex items-end justify-between">
+        <div>
+          {subHeading(List, "Activities")}
+          <ul className="list-disc list-inside ">
+            {activities.map((activity: string) => (
+              <li className={activityTail}>{activity}</li>
+            ))}
+          </ul>
+        </div>
+        <div
+          className="flex items-center border-2 px-1 pr-2.5 py-0.5 rounded-md
+        bg-slate-50 dark:bg-slate-950 text-sky-900 dark:text-cyan-300
+        font-bold  dark:font-medium"
+        >
+          <Hourglass className="p-1.5 mr-1" />
+          <p className="text-sm">{duration}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { CampButton, CampCard };
